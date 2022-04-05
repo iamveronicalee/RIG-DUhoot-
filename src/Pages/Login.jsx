dd81a903aec32d590222bfc48f9816eb6c02f294
adewirya <adewirya88@gmail.com>
2022-04-05 12:01:47 +0700

    fixed login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -14,15 +14,15 @@ export default function Login() {
 
             <form className="space-y-6" action="#" method="POST">
               <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                  Binusian Email
+                <label htmlFor="text" className="block text-sm font-medium text-gray-700">
+                  NIM
                 </label>
                 <div className="mt-1">
                   <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
+                    id="text"
+                    name="text"
+                    type="text"
+                    autoComplete="text"
                     required
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   />
