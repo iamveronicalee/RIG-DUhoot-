c700ae1f3e1faad8000f41b3602ad2458f0e6aac
mavericktherry <48401892+mavericktherry@users.noreply.github.com>
2022-07-16 20:33:48 +0700

    finished new quiz page

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,20 @@ import LoginPage from "./Pages/LoginPage";
 import NewQuizPage from "./Pages/NewQuizPage";
 import MyQuiz from "./Pages/MyQuiz";
 import QuizHistory from "./Pages/QuizHistory";
-
+import TestSocket from "./Pages/TestSocket";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = "/create-quiz" element = {<NewQuizPage/>}></Route>
+        <Route path="/test-socket" element={<TestSocket />}></Route>
+        <Route path="/create-quiz" element={<NewQuizPage />}></Route>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/my-quiz" element={<MyQuiz/>}></Route>
-        <Route path="/quiz-history" element={<QuizHistory/>}></Route>
+        <Route path="/my-quiz" element={<MyQuiz />}></Route>
+        <Route path="/quiz-history" element={<QuizHistory />}></Route>
         <Route path="/auth/login" element={<LoginPage />}></Route>
-        <Route path="*" element={<NotFound/>} />
+        <Route path=""> </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
