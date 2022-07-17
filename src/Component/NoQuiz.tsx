c46f752dc23df2e73d7ce27908d00ec10a58cc82
adewirya <adewirya88@gmail.com>
2022-07-17 19:24:06 +0700

    finished quiz history and my quiz

--- src/Component/NoQuiz.tsx
+++ src/Component/NoQuiz.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import StellarBackground from "./StellarBackground";
+import ReactLoading from "react-loading";
 
-
-export default function NoQuiz(){
-
-    
+export default function NoQuiz() {
+  return (
+    <div>
+      <StellarBackground />
+      <div className="flex justify-center items-center my-64">
+        <div className="flex flex-col justify-center items-center">
+          <ReactLoading type="bubbles" color="#fff" width={'20%'} className="mb-16" />
+          <h1 className="text-white text-center text-5xl font-bold">No Quiz Selected Yet....</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
